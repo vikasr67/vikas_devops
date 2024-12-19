3cfee02f927f793125b4582d15ba3d6bc4c40b72
vikasr67 <vikas67r@gmail.com>
2024-12-19 00:42:47 -0800

    shell files

---
+++ shellscripting/first.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "my name is vikas"
+echo "this is my first shellscrip learnt by abhishek veeramalla"
+
+#create folder 
+mkdir test
+
+#create file 
+cd test
+touch testdone.txt
+echo "hello my boy- enge navu" >> testdone.txt
+echo "He is in class 11th" >> testdone.txt
+echo "he will be writing exam for 10" >> testdone.txt
+
+#run the file 
+cat testdone.txt
---
+++ shellscripting/nodehealth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#################################
+#Auther : Vikas
+#date : 01/12/2024
+
+
+# purpose : to check node health
+#version : v1
+
+##################################
+
+set -x ## this is debug node, it will print command and execution
+
+df -h 
+
+vm_stat
+
+
+
