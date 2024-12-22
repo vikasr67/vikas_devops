70e6b769673c3fd382375b9638ed338e8fc17115
vikaslearner <vikas67r@gmail.com>
2024-12-22 08:17:11 -0800

    pipeline file

---
+++ jenkins/pipe.sh
@@ -0,0 +1,11 @@
+#1/bin/bash
+
+# this is to build git and jenkins pipeline 
+
+echo "On this day, That is $(date '+%Y-%m-%d %H:%M:%S'), pipeline is built"
+
+
+echo "pipeline is wokrking fine and its up and running"
+
+
+
