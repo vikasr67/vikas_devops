66c2bacd63eb577c7c4e36a31922a48966ca156c
vikaslearner <vikas67r@gmail.com>
2025-01-10 04:43:27 -0800

    updated terrraform code

--- terraform/variable_mathod/code.tf
+++ terraform/variable_mathod/code.tf
@@ -65,6 +65,18 @@ resource "azurerm_network_security_group" "prod" {
     source_address_prefix     = "*"
     destination_address_prefix = "*"
   }
+  # Inbound security rule for port 8000 (your application)
+  security_rule {
+    name                       = "Allow-App-8000"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range    = "8000"
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
